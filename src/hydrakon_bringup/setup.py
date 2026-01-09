02422bc5f199e1abb3cc8d071009b4748447597f
Aditya S <as2397@hw.ac.uk>
2026-01-10 00:48:57 +0400

    Added base package structure for Hydrakon

---
+++ src/hydrakon_bringup/setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages, setup
+
+package_name = 'hydrakon_bringup'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Atlas Racing',
+    maintainer_email='atlasracing@hw.ac.uk',
+    description='TODO: Package description',
+    license='MIT',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
